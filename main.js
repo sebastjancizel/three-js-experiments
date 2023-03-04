c50b15838937e470482d3b46416dd9274d5184c9
Sebastjan <sebastjancizel@gmail.com>
2023-03-04 22:41:36 +0000

    Scrolling

--- main.js
+++ main.js
@@ -20,19 +20,32 @@ const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(20, 20, 20);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
+scene.add(pointLight);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
 scene.add(lightHelper);
 
+//set backround color to #757575
+scene.background = new THREE.Color(0x424242);
 
 function animate() {
 	requestAnimationFrame(animate);
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+  //change the disctance of the camera with scroll
 
 	renderer.render(scene, camera);
 }
+
+//add wheel event listener
+document.addEventListener('wheel', (event) => {
+  //check the direction of the scroll and update the camera position accordingly
+  if (event.deltaY > 0) {
+    camera.position.z += 1;
+  } else {
+    camera.position.z -= 1;
+  }
+});
 
 animate();
