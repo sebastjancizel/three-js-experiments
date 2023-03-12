d43d590f61145b8a076dcd4473702bd5322d69a8
Sebastjan <sebastjancizel@gmail.com>
2023-03-12 10:32:08 +0000

    Refactor

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import * as THREE from 'three';
 import Stats from 'three/addons/libs/stats.module.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+import ParticlePlane from './src/particles';
+
 // key particle settings here
 const SEPARATION = 200, AMOUNTX = 100, AMOUNTY = 100;
 const MAX_CAMERA_Y = 200;
@@ -10,7 +12,7 @@ const MAX_CAMERA_Y = 200;
 let container, stats;
 let camera, scene, renderer;
 
-let particles, count = 0;
+let plane, step = 0;
 
 let mouseX = 0, mouseY = 0;
 
@@ -24,60 +26,22 @@ function init() {
 
   container = document.createElement('div');
   document.body.appendChild(container);
-  //make the container fill the right half of the screen
 
-
+  // CAMERA
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 8000);
   camera.position.x = 3000;
   camera.position.y = MAX_CAMERA_Y;
   camera.position.z = 3000;
 
+  // SCENE
   scene = new THREE.Scene();
   // set background color to dark grey
   scene.background = new THREE.Color(0x111111);
 
-  const numParticles = AMOUNTX * AMOUNTY;
+  plane = new ParticlePlane(AMOUNTX, AMOUNTY, SEPARATION);
+  scene.add(plane.particles);
 
-  const positions = new Float32Array(numParticles * 3);
-  const scales = new Float32Array(numParticles);
-
-  let i = 0, j = 0;
-
-  for (let ix = 0; ix < AMOUNTX; ix++) {
-
-    for (let iy = 0; iy < AMOUNTY; iy++) {
-
-      positions[i] = ix * SEPARATION - ((AMOUNTX * SEPARATION) / 2); // x
-      positions[i + 1] = 0; // y
-      positions[i + 2] = iy * SEPARATION - ((AMOUNTY * SEPARATION) / 2); // z
-
-      scales[j] = 20;
-
-      i += 3;
-      j++;
-
-    }
-  }
-
-
-  const geometry = new THREE.BufferGeometry();
-  geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-  geometry.setAttribute('scale', new THREE.BufferAttribute(scales, 1));
-
-  const material = new THREE.ShaderMaterial({
-
-    uniforms: {
-      color: { value: new THREE.Color(0xffffff) },
-    },
-    vertexShader: document.getElementById('vertexshader').textContent,
-    fragmentShader: document.getElementById('fragmentshader').textContent
-
-  });
-
-  particles = new THREE.Points(geometry, material);
-  scene.add(particles);
-
-
+  // RENDERER
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -88,9 +52,6 @@ function init() {
 
   window.addEventListener('resize', onWindowResize);
   window.addEventListener('wheel', onScroll);
-  //add orbit controls
-  // const controls = new OrbitControls(camera, renderer.domElement);
-  // scene.add(controls);
 
   // add axis helper
   const axesHelper = new THREE.AxesHelper(1000);
@@ -122,52 +83,22 @@ function onPointerMove(event) {
 
 function onScroll(event) {
 
-  camera.position.y += event.deltaY * 0.09;
+  camera.position.y = MAX_CAMERA_Y - scrollY;
   camera.position.y = Math.min(camera.position.y, MAX_CAMERA_Y);
 
 }
 
 
 function animate() {
-
   requestAnimationFrame(animate);
-
   render();
-
 }
 
 function render() {
 
-  const positions = particles.geometry.attributes.position.array;
-  const scales = particles.geometry.attributes.scale.array;
-
-  let i = 0, j = 0;
-
-  for (let ix = 0; ix < AMOUNTX; ix++) {
-
-    for (let iy = 0; iy < AMOUNTY; iy++) {
-
-      positions[i + 1] = (Math.sin((ix + count) * 0.3) * 50) +
-        (Math.cos((iy + count) * 0.5) * 50)
-
-      i += 3;
-      j++;
-
-    }
-
-  }
-
-  particles.geometry.attributes.position.needsUpdate = true;
-  particles.geometry.attributes.scale.needsUpdate = true;
-
+  plane.update(step);
   renderer.render(scene, camera);
 
-  count += 0.05;
-
-  // add a constant angular velocity camera rotation around y axis
-  camera.position.x = 3000 * Math.cos(0.01 * count);
-  camera.position.z = 3000 * Math.sin(0.01 * count);
-
+  step += 0.05;
   camera.lookAt(0, camera.position.y, 0);
-
 }
